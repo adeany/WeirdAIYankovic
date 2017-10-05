22e6c153ef047bac85983149272a6e4909a160dc
hawkfalcon <contact@hawkfalcon.com>
2017-10-05 15:52:32 -0700

    Re-add newlines and fix, add better outputting, add new/default arguments.

--- lyric_generator.py
+++ lyric_generator.py
@@ -2,19 +2,19 @@ import random
 import argparse
 from tswift import Artist
 
-start = ">"
-end = "<"
+start = '>'
+end = '<'
+
 
 def get_lyrics(artist_name):
     lyrics = []
     artist = Artist(artist_name)
-    print("Found artist")
+    print('=== Found artist: ', artist_name, ' ===')
     for song in artist.songs:
-        # print(song.lyrics)
-        lyrics.append(song.lyrics.replace("\n", " "))
+        print('Fetched lyrics: ', song.title)
+        lyrics.append(song.lyrics)
 
-    print("Appended all")
-    # print(lyrics)
+    print('=== Fetched all lyrics ===\n')
 
     return lyrics
 
@@ -31,9 +31,9 @@ def train_markov_chain(lyrics):
 
     # print(lyrics)
     for lyric in lyrics:
-        # lyric = lyric.replace("\n", " \n ")
-        words = lyric.split(" ")
-        print(words)
+        lyric = lyric.replace('\n', ' \n ')
+        words = lyric.split(' ')
+
         previous = start
 
         for word in words:
@@ -55,13 +55,16 @@ def generate_new_lyrics(chain):
         current = random.choice(chain[current])
         words.append(current)
 
-    return " ".join(words[:-1])
+    return ' '.join(words[:-1]).replace('\n ', '\n')
 
 
 # parse argument
 parser = argparse.ArgumentParser(description='Generate lyrics.')
-parser.add_argument('name', nargs='*', help='name of artist to search for')
+parser.add_argument('--name', nargs='*',
+                    help='name of artist to search for', default=['Chvrches'])
+parser.add_argument('--save', help='save artist trained markov chain')
 args = parser.parse_args()
+
 artist_name = ' '.join(args.name)
 
 lyrics = get_lyrics(artist_name)
