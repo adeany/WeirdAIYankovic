8e623fff42730f32529efb73378790f61edad861
sonianara <sonaraya@calpoly.edu>
2017-10-24 14:04:23 -0700

    changed pitch

--- changePitch.py
+++ changePitch.py
@@ -1,8 +1,10 @@
 import sox
-
-
+from random import randint
 
 tfm  = sox.Transformer()
-tfm.pitch(5.0)
-tfm.build('./pysox/tests/data/input.wav', 'out.wav')
+
+tfm.bend(5, [1,5,10,15,20], [3,8,14,19,24], [-5, 15, -3, 10, 5], 80, 32)
+tfm.phaser()
+tfm.flanger()
+tfm.build('./marytts/output_wav.wav', 'result.wav')
 
