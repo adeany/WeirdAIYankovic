0f5bfc37d5a141e528b063c2ae7f6a15b1614eb7
David Askari <ddaskari@mailbox.org>
2017-11-06 23:26:40 -0800

    No idea what I'm doing rn.

--- melody-generation/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Required to run it on my machine, sorry (David)
-shopt -s expand_aliases
-source ~/.bashrc
-
-python /char-rnn-tensorflow/sample.py > melody.abc
-python read_abc.py melody.abc --syn_e
