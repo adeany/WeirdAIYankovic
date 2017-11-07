a5518be0c464848b46f47957e3bece92e4ff693d
David Askari <ddaskari@mailbox.org>
2017-11-06 23:15:27 -0800

    Refactor melody generation.

---
+++ melody-generation/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Required to run it on my machine, sorry (David)
+shopt -s expand_aliases
+source ~/.bashrc
+
+python /char-rnn-tensorflow/sample.sh > melody.abc
+python read_abc.py melody.abc --syn_e
