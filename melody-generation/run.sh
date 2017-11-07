5f91a0ccad7d8e39227f7d9717acaa2610098946
bsugiarto24 <bryanick24@yahoo.com>
2017-11-07 15:29:19 -0800

    beginning integration

--- melody-generation/run.sh
+++ melody-generation/run.sh
@@ -4,3 +4,20 @@ cd char-rnn-tensorflow
 python3 sample.py | sed 's/\\n/\n/g' > ../melody.abc
 cat ../melody.abc
 
+
+cd ../abcmidi
+
+make
+
+echo "converting to midi"
+./abc2midi ../melody.abc
+
+brew install timidity
+
+echo "convert to wav"
+timidity melody1.mid -Ow
+
+play melody1.wav
+
+
+
