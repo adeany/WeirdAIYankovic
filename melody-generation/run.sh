78c79c30685bff6d5fda01d2f2e5d3038d2cbd65
bsugiarto24 <bryanick24@yahoo.com>
2017-11-07 16:24:06 -0800

    added key to prepend file

--- melody-generation/run.sh
+++ melody-generation/run.sh
@@ -13,15 +13,12 @@ python3 sample.py  > ../melody1.abc
 
 cd ..
 
-echo -e "X = 1\n$(cat melody1.abc)" > melody2.abc
-
+echo -e "X = 1\n K:D\n$(cat melody1.abc)" > melody2.abc
 sed 's/\\n/\n/g' < melody2.abc > melody.abc
-
 cat melody.abc
 
 
 #convert abc file to wav
-
 ./convert.sh
 
 
