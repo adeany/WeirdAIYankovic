36c9a655356f030b79629c8f44ec26c90ae89f54
Alexandra <adeany@calpoly.edu>
2017-11-29 18:22:25 -0800

    adding code to overlay lyrics and music

---
+++ mixed.py
@@ -0,0 +1,10 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+audio1 = AudioSegment.from_file("./lyric-generation/lyrics_pitched.wav")
+audio2 = AudioSegment.from_file("./melody-generation/melody1.wav")
+
+mixed = audio1.overlay(audio2)
+
+mixed.export("mixed.wav", format='wav')
+play(mixed)
